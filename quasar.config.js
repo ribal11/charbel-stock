8f04c8d2b39cf8e90e57aae571bc00c02d030d8c
elie-asmar <elie.f.asmar@gmail.com>
2023-09-17 02:44:52 +0300

    ok

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/wheibecommerce',
       // analyze: true,
       // env: {},
       // rawDefine: {}
--- src/helpers/globals.js
+++ src/helpers/globals.js
@@ -1,5 +1,6 @@
 const vars = {
-  HomeURL: 'http://127.0.0.1:8000/api',
+  // HomeURL: 'http://127.0.0.1:8000/api',
+  HomeURL: 'https://codefolio.site/wheibecommerce/API/api'
 };
 
 export default vars;
